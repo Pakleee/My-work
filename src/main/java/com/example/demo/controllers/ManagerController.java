5981df46d7fbea982c2180b3924ec60debfb92ae
Pavel <Pavel-kiselev93@mail.ru>
2022-06-04 08:58:20 +0500

    Добавил новый проект, для ДЗ.

---
+++ src/main/java/com/example/demo/controllers/ManagerController.java
@@ -0,0 +1,100 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Post;
+import com.example.demo.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
+public class ManagerController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/manager")
+    public String manager(Model model) {
+        model.addAttribute("title", "Страничка менеджера");
+        return "manager";
+    }
+
+    @GetMapping("/manager/add")
+    public String managerAdd(Model model) {
+        return "manager-add";
+    }
+
+    @PostMapping("/manager/add")
+    public String managerPostAdd(@RequestParam String name, @RequestParam String state, @RequestParam String category, @RequestParam double price, @RequestParam double discount, Model model) {
+        Post post = new Post(name, category, state, price, discount);
+        postRepository.save(post);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/product")
+    public String product(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "product";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/product";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "product-info";
+    }
+
+    @GetMapping("/product/{id}/edit")
+    public String productEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/product";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "product-edit";
+    }
+
+    @PostMapping("/product/{id}/edit")
+    public String managerPostEdit(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String state, @RequestParam String category, @RequestParam double price, @RequestParam double discount, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setName(name);
+        post.setPrice(price);
+        post.setDiscount(discount);
+        post.setCategory(category);
+        post.setDiscount(discount);
+        postRepository.save(post);
+        return "redirect:/product";
+    }
+
+    @PostMapping("/product/{id}/remove")
+    public String managerPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/basket")
+    public String basket(Model model) {
+        model.addAttribute("title", "Корзина");
+        return "basket";
+    }
+
+    @GetMapping("/photo")
+    public String photo(Model model) {
+        model.addAttribute("title", "Лицо сайта");
+        return "photo";
+    }
+}
---
+++ src/main/java/com/example/demo/models/Post.java
@@ -0,0 +1,75 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name,  category, state;
+    private double price, discount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Post() {
+    }
+
+    public Post(String name, String category, String state, double price, double discount) {
+        this.name = name;
+        this.category = category;
+        this.state = state;
+        this.price = price;
+        this.discount = discount;
+    }
+}
